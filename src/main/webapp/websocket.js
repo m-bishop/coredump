7777319b28169196b51deb5f229f01ad717c6e0f
bishop <coredumpproject@gmail.com>
2017-05-22 20:35:12 -0400

    changed websocket address

--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -1,4 +1,4 @@
-var wsUri = "ws://" + document.location.hostname + document.location.pathname + "chat"; 
+var wsUri = "ws://" + document.location.hostname + document.location.pathname + "chat";
 var websocket = new WebSocket(wsUri);
 
 var username;
